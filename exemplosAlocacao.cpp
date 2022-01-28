6ebc5be5a7a0a7b260add5b8d47b52d93942a974
hugoresende27 <Hugoresende27@gmail.com>
2022-01-28 16:19:05 +0000

    a

---
+++ exemplosAlocacao.cpp
@@ -0,0 +1,28 @@
+#include <iostream>	
+#include <string.h>
+#include <math.h>		
+#include <locale.h>
+#include <fstream>
+using namespace std;
+
+int main()								
+{
+
+    int n;
+    printf ("Tamanho do array->");
+    scanf("%d",&n);
+
+    int *ptArr = (int*)malloc(n*sizeof(int));//array alocado dinamicamente
+    //preencher o array com valores a começar no 1
+    for (int i=0; i<n; i++){
+        ptArr[i] = i+1;   
+    }
+    //imprimir o array
+    for (int i=0; i<n; i++){
+        printf("%d - ",ptArr[i]);
+    }
+
+    cout << endl;
+    system("pause");				
+	return 0;	
+}
